5b11602e0169ec401ac411a85dfcaf3529244a65
hello <54324859+AvalonsLake@users.noreply.github.com>
2025-12-19 10:43:45 -0700

    Day21

--- week3/Day21/main.py
+++ week3/Day21/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -11,22 +12,35 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
+screen.onkey(snake.up, "w")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.left, "a")
+screen.onkey(snake.right, "d")
 
 game_is_on = True
 while game_is_on:
     screen.update()
+    scoreboard.write_score()
     time.sleep(0.1)
     snake.move()
 
 #     Detecting collision with the food buffs
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
+        scoreboard.get_point()
+
+#     hitting a wall!
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
 
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 screen.exitonclick()
