2f8413af5b349d91e482be36cb16308367de6a64
JamesHendricks <54324859+AvalonsLake@users.noreply.github.com>
2026-01-08 12:39:27 -0700

    Update Day26

--- week3/Day26/main.py
+++ week3/Day26/main.py
@@ -1,31 +1,20 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
 import pandas
-# student_data_frame = pandas.DataFrame(student_dict)
+
 nato_alphabet_data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-#Loop through rows of a data frame
-# for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    # pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
-#TODO 1. Create a dictionary in this format:
 nato_alphabet = {row.letter:row.code for (index, row) in nato_alphabet_data.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def convert_to_nato():
+    user_word = input("Enter a word: ").upper()
+    try:
+        if user_word == "":
+            print("Sorry, you must enter a word")
+            convert_to_nato()
+        nato_list = [nato_alphabet[letter] for letter in user_word]
+    except KeyError:
+        print(f"sorry, {user_word} is not a valid word")
+        convert_to_nato()
+    else:
+        print(nato_list)
 
-user_word = input("Enter a word: ").upper()
-nato_list = [nato_alphabet[letter] for letter in user_word]
-print(nato_list)
+convert_to_nato()
---
+++ week3/Day30/main.py
@@ -0,0 +1,81 @@
+from tkinter import *
+from tkinter import messagebox as mbox, messagebox
+from random import choice, randint, shuffle
+import pyperclip
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_random_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    pass_entry.delete(0, END)
+    pass_entry.insert(END, password)
+    pyperclip.copy(password)
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def save_pass():
+    website = website_entry.get()
+    user = user_entry.get()
+    password = pass_entry.get()
+
+    if len(website) == 0 or len(user) == 0 or len(password) == 0:
+        messagebox.showerror("Error", "Please fill in all fields")
+        return
+
+    is_ok = mbox.askokcancel("Are You Sure?", f"This is what you entered: \n"
+                                      f"website: {website}\nemail: {user}\npassword: {password}\n"
+                                      f"Do you want to save this?")
+    if is_ok:
+        with open("data.txt", "a") as data:
+            data.write(f"{website} | {user} | {password}\n")
+            website_entry.delete(0, END)
+            pass_entry.delete(0, END)
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager for the Mentally Overworked")
+window.config(padx=40, pady=40)
+window.minsize(200, 200)
+
+canvas = Canvas(window, width=200, height=200, highlightthickness=0)
+LOGO = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=LOGO)
+canvas.grid(row=0, column=1)
+
+# Labels
+website_label = Label(window, text="Website:")
+website_label.grid(row=1, column=0)
+user_label = Label(window, text="Email/Username:")
+user_label.grid(row=2, column=0)
+pass_label = Label(window, text="Password:")
+pass_label.grid(row=3, column=0)
+
+# Entries
+website_entry = Entry(window, width=54)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
+user_entry = Entry(window, width=54)
+user_entry.grid(row=2, column=1, columnspan=2)
+user_entry.insert(END, "jlh3ndr@gmail.com")
+pass_entry = Entry(window, width=35)
+pass_entry.grid(row=3, column=1)
+
+# Buttons
+pass_btn = Button(window, text="Generate Password", command=generate_random_password)
+pass_btn.grid(row=3, column=2, columnspan=2)
+add_btn = Button(window, text="Add", width=46, command=save_pass)
+add_btn.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
