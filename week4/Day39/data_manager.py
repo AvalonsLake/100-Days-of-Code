7a7cb48e61eb5fb1a125b8d7a88a1defe881619d
JamesHendricks <54324859+AvalonsLake@users.noreply.github.com>
2026-01-20 11:42:56 -0700

    Day39

--- week4/Day39/data_manager.py
+++ week4/Day39/data_manager.py
@@ -1,3 +1,33 @@
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+from dotenv import load_dotenv
+from pprint import pprint
+
+load_dotenv()
+
+sheety_ep = os.getenv("SHEETY_EP")
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=sheety_ep)
+        destination = response.json()
+        self.destination_data = destination["prices"]
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{sheety_ep}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
--- week4/Day39/notification_manager.py
+++ week4/Day39/notification_manager.py
@@ -1,3 +1,29 @@
+import os
+from twilio.rest import Client
+
+# Using a .env file to retrieve the phone numbers and tokens.
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+
+    def __init__(self):
+        self.client = Client(os.getenv('TWILIO_SID'), os.getenv("TWILIO_AUTH_TOKEN"))
+
+    def send_sms(self, message_body):
+
+        message = self.client.messages.create(
+            from_=os.getenv("TWILIO_VIRTUAL_NUMBER"),
+            body=message_body,
+            to=os.getenv("MY_NUMBER")
+        )
+        # Prints if successfully sent.
+        print(message.sid)
+
+    # Is SMS not working for you or prefer whatsapp? Connect to the WhatsApp Sandbox!
+    # https://console.twilio.com/us1/develop/sms/try-it-out/whatsapp-learn
+    def send_whatsapp(self, message_body):
+        message = self.client.messages.create(
+            from_=f'whatsapp:{os.getenv("TWILIO_WHATSAPP_NUMBER")}',
+            body=message_body,
+            to=f'whatsapp:{os.getenv("TWILIO_VERIFIED_NUMBER")}'
+        )
+        print(message.sid)
